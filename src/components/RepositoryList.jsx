c881d71fb29a01a9cfd5346d616c7e869b8b2fbf
Felizardo <devfelizardo@gmail.com>
2021-08-04 01:07:40 -0300

    adding proprerties to the repository list and itens

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,27 +1,19 @@
-const repositoryName = "Sample 2";
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "Sample 1",
+  description: "Sample form description",
+  link: "https://github.com/unform/unform",
+};
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Repository List</h1>
       <ul>
-        <li>
-          <strong>Repo Name Sample</strong>
-          <p>Sample for description for that repository</p>
-          <a href="">Access Repo in Github</a>
-        </li>
-
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Sample for description for that repository</p>
-          <a href="">Access Repo in Github</a>
-        </li>
-
-        <li>
-          <strong>Repo Name Sample</strong>
-          <p>Sample for description for that repository</p>
-          <a href="">Access Repo in Github</a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
